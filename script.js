e90e67a8d6cf5bde38afa70e8e172dc3e0def7b5
Sourav Giri <computerscienceengineer1@gmail.com>
2026-01-21 10:39:05 +0530

    phase 2 completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const rectBtn = document.querySelector("#rectangleIcon");
+const textBtn = document.querySelector("#textIcon")
 const canvasAreaSection = document.querySelector("#canvas")
 
 
@@ -10,6 +11,9 @@ const randomId = (length = 6) => {
 const elements = [];
 let selectedElementId = null;
 const base = 40;
+
+
+// functionality add on click on rectangle button
 
 rectBtn.addEventListener("click",(e)=>{
   
@@ -26,6 +30,7 @@ rectBtn.addEventListener("click",(e)=>{
   div.style.position = "absolute";
   div.style.top = `${y}px`;
   div.style.left = `${x}px`;
+  div.style.fontSize = "16px";
 
   div.id = id;
   div.dataset.type = "rect"
@@ -34,4 +39,37 @@ rectBtn.addEventListener("click",(e)=>{
   canvasAreaSection.append(div)
   elements.push({id,type:"rect",x,y,width:100,height:80,bgColor:"#4f8cff"})
   
+})
+
+
+
+// functionality add on click on text  button
+
+textBtn.addEventListener("click",(e)=>{
+
+  const id = randomId(10);
+  const offset = elements.length * 20;
+  const x = offset+base;
+  const y = offset+base
+
+  const div = document.createElement("div");
+  div.textContent = "default text generation"
+  div.style.height = "30px";
+  div.style.width = "190px";
+  div.style.backgroundColor = "transparent";
+  div.style.color=  'white'
+  div.style.position = "absolute";
+  div.style.top = `${y}px`;
+  div.style.left = `${x}px`;
+  div.style.cursor = 'text';
+  div.contentEditable = true;
+
+
+  div.id = id;
+  div.dataset.type = "text"
+
+  selectedElementId = div.id;
+  canvasAreaSection.append(div)
+  elements.push({id,type:"text",x,y,width:190,height:30,bgColor:"transparent"})
+
 })
